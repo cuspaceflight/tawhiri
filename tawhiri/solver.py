dc232956509a1888e871b1742c17b90904de7d1b
Adam Greig <adam@adamgreig.com>
2014-02-25 17:27:55 +0000

    use list of (f, term)s

--- tawhiri/solver.py
+++ tawhiri/solver.py
@@ -1,5 +1,12 @@
+def solve(t, lat, lng, alt, fs, terms, dt):
+    results = [(t, lat, lng, alt)]
+    for f, term in zip(fs, terms):
+        results += euler(t, lat, lng, alt, f, term, dt)
+        t, lat, lng, alt = results[-1]
+    return results
+
 def euler(t, lat, lng, alt, f, terminator, dt):
-    result = [(t, lat, lng, alt)]
+    result = []
     while not terminator(t, lat, lng, alt):
         t += dt
         df = f(t, lat, lng, alt)
@@ -7,5 +14,4 @@ def euler(t, lat, lng, alt, f, terminator, dt):
         lng += df[1] * dt
         alt += df[2] * dt
         result.append((t, lat, lng, alt))
-
     return result
--- tests/run_prediction.py
+++ tests/run_prediction.py
@@ -6,7 +6,6 @@ ds = wind.Dataset("/home/adam/Projects/tawhiri/datasets", 2014, 2, 3, 6)
 f = models.make_f([models.make_constant_ascent(5.0), models.wind_velocity], ds)
 term = models.make_burst_termination(30000.0)
 
-
 t0 = 6.0 * 3600
 lat0 = 52.0
 lng0 = 0.0
@@ -18,7 +17,7 @@ n_repeats = 100
 
 start_time = time.time()
 for i in range(n_repeats):
-    result = solver.euler(t0, lat0, lng0, alt0, f, term, dt)
+    result = solver.solve(t0, lat0, lng0, alt0, [f], [term], dt)
 end_time = time.time()
 
 #for idx, t in enumerate(ts):
