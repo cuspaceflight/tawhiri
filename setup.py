b143b39ad89a6bad6009aa13ff45cce56817ed9b
Adam Greig <adam@adamgreig.com>
2014-04-07 15:44:59 +0100

    Fill out setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,40 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = ''
+
+def get_version():
+    with open("tawhiri/__init__.py") as f:
+        for line in f:
+            if line.startswith("__version__"):
+                return line[15:-2]
+    raise Exception("Could not find version number")
+
 setup(
     name="Tawhiri",
-    ext_modules = cythonize("tawhiri/*.pyx")
+    version=get_version(),
+    author='Cambridge University Spaceflight',
+    author_email='contact@cusf.co.uk',
+    packages=['tawhiri'],
+    ext_modules = cythonize("tawhiri/*.pyx"),
+    url='http://www.cusf.co.uk/wiki/tawhiri:start',
+    licence='GPLv3+',
+    description='High Altitude Balloon Landing Prediction Software',
+    long_description=long_description,
+    test_suite='nose.collector',
+    tests_require=['nose', 'mock'],
+    install_requires=[
+        "Cython==0.20.1",
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Scientific/Engineering',
+    ],
 )
---
+++ tawhiri/__init__.py
@@ -0,0 +1,27 @@
+# Copyright 2014 (C) Adam Greig
+#
+# This file is part of Tawhiri.
+#
+# habitat is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# habitat is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with habitat.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Tawhiri is trajectory prediction software for high altitude balloons.
+
+See http://www.cusf.co.uk/wiki/tawhiri:start for further details.
+"""
+
+__author__ = "Cambridge University Spaceflight <contact@cusf.co.uk>"
+__version__ = "0.1.0"
+__version_info__ = tuple([int(d) for d in __version__.split(".")])
+__licence__ = "GPL v3"
