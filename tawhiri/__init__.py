b34ab42f3bf8b325b7580a091d9fca59974df475
Daniel Richman <main@danielrichman.co.uk>
2014-04-08 13:45:13 +0100

    Fix habitat->Tawhiri in copyright notices

--- tawhiri/__init__.py
+++ tawhiri/__init__.py
@@ -2,18 +2,18 @@
 #
 # This file is part of Tawhiri.
 #
-# habitat is free software: you can redistribute it and/or modify
+# Tawhiri is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# habitat is distributed in the hope that it will be useful,
+# Tawhiri is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with habitat.  If not, see <http://www.gnu.org/licenses/>.
+# along with Tawhiri.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 Tawhiri is trajectory prediction software for high altitude balloons.
