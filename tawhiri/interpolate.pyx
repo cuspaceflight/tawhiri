2419ddc628752799091aef7f5a6c2a65bf07e764
Adam Greig <adam@adamgreig.com>
2014-04-10 18:34:49 +0100

    Proxy access to mmap, closes #19 and closes #17

--- tawhiri/interpolate.pyx
+++ tawhiri/interpolate.pyx
@@ -33,6 +33,9 @@ Note that this module is compiled with Cython to enable fast
 memory access.
 """
 
+from cpython.buffer cimport PyBUF_SIMPLE, Py_buffer, PyObject_GetBuffer
+cdef extern int PyObject_AsReadBuffer(object, const void **, Py_ssize_t *)
+
 
 # These need to match Dataset.axes.variable
 DEF VAR_A = 0
@@ -51,10 +54,47 @@ cdef struct Lerp3:
     double lerp
 
 
+cdef class DatasetProxy:
+    cdef void* buf
+    cdef Py_ssize_t len
+    cdef Py_ssize_t[5] shape
+    cdef Py_ssize_t[5] strides
+
+    def __init__(object self, object memmap):
+        cdef Py_buffer pyb
+        cdef const void * cbuf = <const void*> self.buf
+        IF PY2:
+            PyObject_AsReadBuffer(memmap, &cbuf, &self.len)
+            self.buf = <void*>cbuf
+        ELSE:
+            PyObject_GetBuffer(memmap, &pyb, PyBUF_SIMPLE)
+            self.buf = pyb.buf
+            self.len = pyb.len
+
+        shape = (65, 47, 3, 361, 720)
+        for idx, val in enumerate(shape):
+            self.shape[idx] = val
+        for idx, val in enumerate(shape):
+            acc = 8
+            for val in shape[idx+1:]:
+                acc *= val
+            self.strides[idx] = acc
+
+    def __getbuffer__(object self, Py_buffer* view, int flags):
+        view.buf = self.buf
+        view.len = self.len
+        view.shape = self.shape
+        view.strides = self.strides
+        view.readonly = 0
+        view.format = "d"
+        view.itemsize = 8
+        view.ndim = 5
+
+
 def make_interpolator(dataset):
     cdef double[:, :, :, :, :] data
 
-    data = memoryview(dataset.array).cast("d", (65, 47, 3, 361, 720))
+    data = DatasetProxy(dataset.array)
 
     def f(hour, alt, lat, lng):
         return get_wind(data, hour, alt, lat, lng)
