d2352be054708fae334578f3720deec044304ab3
Daniel Richman <main@danielrichman.co.uk>
2014-08-05 22:11:56 +0100

    Indentation in kml.py

--- tawhiri/kml.py
+++ tawhiri/kml.py
@@ -3,8 +3,8 @@ import jinja2
 def kml(points, markers, filename=None):
     loader = jinja2.PackageLoader('tawhiri', '')
     env = jinja2.Environment(loader=loader,
-            extensions=['jinja2.ext.autoescape'],
-            undefined=jinja2.StrictUndefined)
+                             extensions=['jinja2.ext.autoescape'],
+                             undefined=jinja2.StrictUndefined)
 
     template = env.get_template("template.kml")
     kwargs = {'points': points, 'markers': markers}
