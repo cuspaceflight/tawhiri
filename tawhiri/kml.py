d1eff579238125b9b2cefc53ba0c54c30cf27ad8
Daniel Richman <main@danielrichman.co.uk>
2014-08-12 21:28:28 +0100

    Return the results from each prediction stage separately

--- tawhiri/kml.py
+++ tawhiri/kml.py
@@ -6,7 +6,12 @@ env = jinja2.Environment(loader=loader,
                          undefined=jinja2.StrictUndefined)
 template = env.get_template("template.kml")
 
-def kml(points, markers, filename=None):
+def kml(stages, markers, filename=None):
+    points = stages[0]
+    for stage in stages[1:]:
+        assert points[-1] == stage[0]
+        points += stage[1:]
+
     kwargs = {'points': points, 'markers': markers}
     if filename:
         template.stream(**kwargs).dump(filename)
