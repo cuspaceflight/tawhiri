d2f4a53aa22799749a1dfa63f266d3dc98b52d74
Jamie Wood <jamie.wood70@ntlword.com>
2015-03-19 03:49:31 +0000

    More UI

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,38 @@ function init(){
 	var map = new google.maps.Map(document.getElementById("map-container"), {
 		zoom: 8,
 		center: new google.maps.LatLng(52.0, 0)
+	});
+
+	$("#sidebar-toggle").click(function(){
+		var sidebar = $(".sidebar");
+		if(sidebar.css("left")!="0px")
+			sidebar.css("left", "0px");
+		else
+			sidebar.css("left", "-330px");
 	})
+
+	$('.unit-selection .dropdown-menu li a').click(function(event) {
+            event.preventDefault();
+            var $unit = $(this);
+            var $unit_selection = $unit.closest('.unit-selection');
+            $unit_selection.find('.unit-current').html($unit.html());
+            $unit_selection.find('input[type="hidden"]').val($unit.html());
+            $unit_selection.click();
+            return false;
+        });
+
+	$("#inputLaunchDate").datetimepicker({
+		timepicker:false,
+		format:"Y/m/d"
+	});
+
+	$("#inputLaunchStartTime").datetimepicker({
+		datepicker:false,
+		format:"H:i"
+	});
+	$("#inputLaunchEndTime").datetimepicker({
+		datepicker:false,
+		format:"H:i"
+	});
+
 }
